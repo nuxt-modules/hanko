691310045ca6c7e09873dc160a8813179a21bb12
Daniel Roe <daniel@roe.dev>
2023-06-05 21:34:33 +0100

    test: add type test

--- src/runtime/server/middleware/auth.ts
+++ src/runtime/server/middleware/auth.ts
@@ -1,5 +1,6 @@
 import type { JWTPayload } from 'jose'
-import { defineEventHandler, verifyHankoEvent } from '#imports'
+import { defineEventHandler } from 'h3'
+import { verifyHankoEvent } from '../utils/index'
 
 export default defineEventHandler(async event => {
   event.context.hanko = await verifyHankoEvent(event).catch(() => undefined)
