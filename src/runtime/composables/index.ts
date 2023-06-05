52d3bc5ae018e0362f4097d664359574f6d71830
Daniel Roe <daniel@roe.dev>
2023-06-06 00:04:23 +0100

    docs: add comment

--- src/runtime/composables/index.ts
+++ src/runtime/composables/index.ts
@@ -1,6 +1,11 @@
 import { useRuntimeConfig } from '#imports'
 import { Hanko } from '@teamhanko/hanko-elements'
 
+/**
+ * This composable returns a Hanko instance.
+ *
+ * It will be `null` on the server but defined on the client.
+ */
 export function useHanko() {
   if (process.server) {
     return null
