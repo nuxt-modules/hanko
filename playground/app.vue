bd83d1ef2fb56a4dcd836e5288b15a7fced83b17
McPizza <o@l1.io>
2023-06-07 13:33:10 +0100

    Add new links to nav

--- playground/app.vue
+++ playground/app.vue
@@ -1,14 +1,10 @@
 <template>
   <nav>
-    <NuxtLink to="/">
-      Go home
-    </NuxtLink>
-    <NuxtLink to="/about">
-      About page
-    </NuxtLink>
-    <NuxtLink to="/login">
-      Log in page
-    </NuxtLink>
+    <NuxtLink to="/"> Go home </NuxtLink>
+    <NuxtLink to="/login"> Log in page ⛔️👤 </NuxtLink>
+    <NuxtLink to="/user"> User Page 🔐 </NuxtLink>
+    <NuxtLink to="/protected"> Protected page 🔐 </NuxtLink>
+    <NuxtLink to="/about"> About page </NuxtLink>
   </nav>
   <NuxtPage />
 </template>
