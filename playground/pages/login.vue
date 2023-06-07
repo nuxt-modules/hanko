cc2bb39ce92de10c3e287294ee9ef70a54dac207
McPizza <o@l1.io>
2023-06-07 13:34:28 +0100

    add login page helper text

--- playground/pages/login.vue
+++ playground/pages/login.vue
@@ -1,13 +1,23 @@
-
-<template>
-  <main>
-    <h1>Log in</h1>
-    <hanko-auth />
-  </main>
-</template>
-
 <script setup lang="ts">
 definePageMeta({
   middleware: ['hanko-logged-out']
 })
+const redirectPath = ref<string | null>(null)
+redirectPath.value = useRoute().query.redirect as string | null
 </script>
+
+<template>
+  <main>
+    <h1>Log in</h1>
+    <p>
+      Only logged out users can see this page<pre>definePageMeta({
+  middleware: ['hanko-logged-out'],
+})</pre>
+    </p>
+    <p v-if="$route.query.redirect">
+      You were redirected here from {{ $route.query.redirect }}, once you login, you'll be sent back automatically!
+    </p>
+    <hanko-auth />
+  </main>
+</template>
+
