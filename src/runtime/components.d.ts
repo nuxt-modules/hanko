69e65f0eca29b3c073e3c59cf64ed9db60f6971c
Felix Dolderer <mail@felixdolderer.com>
2025-06-12 15:02:24 +0200

    improve custom meta type

--- src/runtime/components.d.ts
+++ src/runtime/components.d.ts
@@ -26,6 +26,9 @@ declare module 'nuxt/app' {
   interface PageMeta {
     hanko?: {
       allow?: 'all' | 'logged-in' | 'logged-out'
+      deny?: never
+    } | {
+      allow?: never
       deny?: 'logged-in' | 'logged-out'
     }
   }
