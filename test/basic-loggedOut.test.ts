bc359c82204acb9c292ce27d8ae7d39efac73086
Felix Dolderer <mail@felixdolderer.com>
2025-06-14 20:19:17 +0200

    basic loggedOut ssr test

---
+++ test/basic-loggedOut.test.ts
@@ -0,0 +1,37 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, fetch, $fetch } from '@nuxt/test-utils'
+
+await setup({
+  rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+})
+
+describe('No global middleware, not logged in', async () => {
+  describe('ssr', () => {
+    it('hanko-logged-in middleware redirects to the login page', async () => {
+      const res = await fetch('/protected', { redirect: 'manual' })
+      expect(res.headers.get('location')).toBe('/login?redirect=/protected')
+    })
+
+    it('hanko-logged-out middleware renders page', async () => {
+      const res = await fetch('/login', { redirect: 'manual' })
+      expect(res.redirected).toBeFalsy()
+    })
+
+    it('renders page without middleware', async () => {
+      const res = await fetch('/about', { redirect: 'manual' })
+      expect(res.redirected).toBeFalsy()
+    })
+
+    it('respects custom elements', async () => {
+      const html = await $fetch('/login')
+      expect(html).toContain('<hanko-auth></hanko-auth>')
+    })
+  })
+
+  describe('client', () => {
+    it.todo('middleware:none')
+    it.todo('middleware:loggedIn')
+    it.todo('middleware:loggedOut')
+  })
+})
