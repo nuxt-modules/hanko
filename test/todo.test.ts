a38504245e87a30c7fe315dc4c351d1595d8f2b9
Felix Dolderer <mail@felixdolderer.com>
2025-06-14 19:25:10 +0200

    test plan

---
+++ test/todo.test.ts
@@ -0,0 +1,103 @@
+import { describe, test } from 'vitest'
+
+describe('Without globalMiddleware', () => {
+  describe('loggedOut', () => {
+    describe('ssr', () => {
+      test.todo('middleware:none')
+      test.todo('middleware:loggedIn')
+      test.todo('middleware:loggedOut')
+    })
+    describe('client', () => {
+      test.todo('middleware:none')
+      test.todo('middleware:loggedIn')
+      test.todo('middleware:loggedOut')
+    })
+  })
+
+  describe('loggedIn', () => {
+    describe('ssr', () => {
+      test.todo('middleware:none')
+      test.todo('middleware:loggedIn')
+      test.todo('middleware:loggedOut')
+    })
+    describe('client', () => {
+      test.todo('middleware:none')
+      test.todo('middleware:loggedIn')
+      test.todo('middleware:loggedOut')
+    })
+  })
+
+  test.todo('pageMeta has no effect')
+})
+
+describe('With globalMiddleware', () => {
+  describe('loggedOut', () => {
+    describe('ssr', () => {
+      describe('middleware', () => {
+        test.todo('none')
+        test.todo('loggedIn')
+        test.todo('loggedOut')
+      })
+
+      describe('pageMeta', () => {
+        test.todo('allow:all')
+        test.todo('allow:logged-in')
+        test.todo('allow:logged-out')
+        test.todo('deny:logged-in')
+        test.todo('deny:logged-out')
+      })
+    })
+
+    describe('client', () => {
+      describe('middleware', () => {
+        test.todo('none')
+        test.todo('loggedIn')
+        test.todo('loggedOut')
+      })
+
+      describe('pageMeta', () => {
+        test.todo('allow:all')
+        test.todo('allow:logged-in')
+        test.todo('allow:logged-out')
+        test.todo('deny:logged-in')
+        test.todo('deny:logged-out')
+      })
+    })
+  })
+
+  describe('loggedIn', () => {
+    describe('ssr', () => {
+      describe('middleware', () => {
+        test.todo('none')
+        test.todo('loggedIn')
+        test.todo('loggedOut')
+      })
+
+      describe('pageMeta', () => {
+        test.todo('allow:all')
+        test.todo('allow:logged-in')
+        test.todo('allow:logged-out')
+        test.todo('deny:logged-in')
+        test.todo('deny:logged-out')
+      })
+    })
+
+    describe('client', () => {
+      describe('middleware', () => {
+        test.todo('none')
+        test.todo('loggedIn')
+        test.todo('loggedOut')
+      })
+
+      describe('pageMeta', () => {
+        test.todo('allow:all')
+        test.todo('allow:logged-in')
+        test.todo('allow:logged-out')
+        test.todo('deny:logged-in')
+        test.todo('deny:logged-out')
+      })
+    })
+  })
+
+  test.todo('middleware trumps pageMeta')
+})
