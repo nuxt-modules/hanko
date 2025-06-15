0ef804450f95125f5684cd94b63a769e18e0d9a5
Felix Dolderer <mail@felixdolderer.com>
2025-06-15 16:38:36 +0200

    add remaining tests

--- test/client/globalMiddleware-loggedIn-client.test.ts
+++ test/client/globalMiddleware-loggedIn-client.test.ts
@@ -1,4 +1,5 @@
-import { $fetch, fetch, setup } from '@nuxt/test-utils'
+import { setup } from '@nuxt/test-utils'
+import { createPage, useTestContext } from '@nuxt/test-utils/e2e'
 import defu from 'defu'
 import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
@@ -10,19 +11,59 @@ await setup({
 })
 
 describe('Global middleware, logged in, client-side', async () => {
-  it('renders page with no middleware')
+  it('renders page with no middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=About page')
+    await page.waitForURL(`${useTestContext().url}about`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}about`)
+  })
 
-  it('renders page with hanko-logged-in middleware')
+  it('renders page with hanko-logged-in middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=Protected page 🔐')
+    await page.waitForURL(`${useTestContext().url}protected`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}protected`)
+  })
 
-  it('redirects to home page for hanko-logged-out middleware')
+  it('redirects to home page for hanko-logged-out middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=Log in page ⛔️👤')
+    await page.waitForURL(`${useTestContext().url}`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}`)
+  })
 
-  it('allow:logged-in renders page')
+  it('allow:all renders page', async () => {
+    const page = await createPage('/about')
+    await page.click('#allow-all')
+    await page.waitForURL(`${useTestContext().url}global/allow/all`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}global/allow/all`)
+  })
 
-  it('allow:logged-in renders page')
+  it('allow:logged-in renders page', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('#allow-logged-in')
+    await page.waitForURL(`${useTestContext().url}global/allow/logged-in`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}global/allow/logged-in`)
+  })
 
-  it('allow:logged-out redirects to home page')
+  it('allow:logged-out redirects to home page', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('#allow-logged-out')
+    await page.waitForURL(`${useTestContext().url}`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}`)
+  })
 
-  it('deny:logged-in redirects to home page')
+  it('deny:logged-in redirects to home page', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('#deny-logged-in')
+    await page.waitForURL(`${useTestContext().url}`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}`)
+  })
 
-  it('deny:logged-out renders page')
+  it('deny:logged-out renders page', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('#deny-logged-out')
+    await page.waitForURL(`${useTestContext().url}global/deny/logged-out`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}global/deny/logged-out`)
+  })
 })
