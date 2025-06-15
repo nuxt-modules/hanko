0337bc26cad4285688384f642e1d48a15a103659
Felix Dolderer <mail@felixdolderer.com>
2025-06-15 15:51:03 +0200

    basic loggedOut client test

--- test/client/basic-loggedOut-client.test.ts
+++ test/client/basic-loggedOut-client.test.ts
@@ -1,17 +1,38 @@
+import { setup } from '@nuxt/test-utils'
+import { createPage, useTestContext } from '@nuxt/test-utils/e2e'
 import { fileURLToPath } from 'node:url'
-import { describe, it, expect } from 'vitest'
-import { setup, fetch, $fetch } from '@nuxt/test-utils'
+import { describe, expect, it } from 'vitest'
 
 await setup({
   rootDir: fileURLToPath(new URL('../../playground', import.meta.url)),
 })
 
 describe('No global middleware, not logged in, client-side', async () => {
-  it('pageMeta:hanko has no effect without global middleware', async () => {})
+  it('pageMeta:hanko has no effect without global middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('#allow-logged-in')
+    await page.waitForURL(`${useTestContext().url}global/allow/logged-in`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}global/allow/logged-in`)
+  })
 
-  it('renders page with no middleware', async () => {})
+  it('renders page with no middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=About page')
+    await page.waitForURL(`${useTestContext().url}about`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}about`)
+  })
 
-  it('redirects to login page for hanko-logged-in middleware', async () => {})
+  it('redirects to login page for hanko-logged-in middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=Protected page 🔐')
+    await page.waitForURL(`${useTestContext().url}login?redirect=/protected`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}login?redirect=/protected`)
+  })
 
-  it('renders page with hanko-logged-out middleware', async () => {})
+  it('renders page with hanko-logged-out middleware', async () => {
+    const page = await createPage('/global/allow/all')
+    await page.click('text=Log in page ⛔️👤')
+    await page.waitForURL(`${useTestContext().url}login`, { timeout: 1000 })
+    expect(page.url()).toBe(`${useTestContext().url}login`)
+  })
 })
