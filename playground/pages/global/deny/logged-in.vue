7dfb1309635e4f879416c07bb0f00bb637f6bbd5
Felix Dolderer <mail@felixdolderer.com>
2026-02-28 21:24:01 +0100

    feat: add global middleware support and enhance route protection
    - Introduced `globalMiddleware` option in the Nuxt configuration to protect all pages by default.
    - Added new route middleware for handling access based on user authentication status.
    - Updated README to include details on global middleware usage and configuration.
    - Created example pages demonstrating the use of `allow` and `deny` settings in pageMeta.
    - Enhanced test coverage for global middleware scenarios, including client-side and SSR tests.

---
+++ playground/pages/global/deny/logged-in.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  hanko: {
+    deny: 'logged-in',
+  },
+})
+</script>
+
+<template>
+  <main>
+    <h1>GlobalMiddleware: deny loggedIn</h1>
+    <p>
+      This page is not accessible to loggedIn users <pre>definePageMeta({
+  hanko: {
+    deny: 'logged-in',
+  }
+})</pre>
+    </p>
+  </main>
+</template>
