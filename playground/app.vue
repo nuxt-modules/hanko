7e737de6d363e0e9a4c063951b15b1d254158b28
Felix Dolderer <mail@felixdolderer.com>
2026-03-01 00:14:00 +0100

    feat: enhance navigation with test environment support
    - Added a computed property to check if the application is running in a test environment.
    - Updated the navigation structure in `app.vue` to conditionally render links based on the test environment and global middleware state.

--- playground/app.vue
+++ playground/app.vue
@@ -1,63 +1,67 @@
 <script setup lang="ts">
 const nuxtConfig = useAppConfig()
 const isGlobalMiddleware = computed(() => nuxtConfig.hanko.globalMiddleware)
+const isTest = computed(() => process.env.VITEST === 'true')
 </script>
 
 <template>
-  <nav v-if="!isGlobalMiddleware">
-    <NuxtLink to="/">
-      Go home
-    </NuxtLink>
-    <NuxtLink to="/login">
-      Log in page ⛔️👤
-    </NuxtLink>
-    <NuxtLink to="/user">
-      User Page 🔐
-    </NuxtLink>
-    <NuxtLink to="/protected">
-      Protected page 🔐
-    </NuxtLink>
-    <NuxtLink to="/about">
-      About page
-    </NuxtLink>
-  </nav>
-  <nav v-else>
-    <NuxtLink
-      id="allow-all"
-      to="/global/allow/all"
-    >
-      Allow all
-    </NuxtLink>
-    <NuxtLink
-      id="allow-logged-in"
-      to="/global/allow/logged-in"
-    >
-      Allow logged-in
-    </NuxtLink>
-    <NuxtLink
-      id="allow-logged-out"
-      to="/global/allow/logged-out"
-    >
-      Allow logged-out
-    </NuxtLink>
-    <NuxtLink
-      id="deny-logged-in"
-      to="/global/deny/logged-in"
-    >
-      Deny logged-in
-    </NuxtLink>
-    <NuxtLink
-      id="deny-logged-out"
-      to="/global/deny/logged-out"
-    >
-      Deny logged-out
-    </NuxtLink>
-    <NuxtLink
-      id="incorrect-usage"
-      to="/global/incorrect-usage"
-    >
-      Incorrect usage
-    </NuxtLink>
+  <nav>
+    <template v-if="isTest || !isGlobalMiddleware">
+      <NuxtLink to="/">
+        Go home
+      </NuxtLink>
+      <NuxtLink to="/login">
+        Log in page ⛔️👤
+      </NuxtLink>
+      <NuxtLink to="/user">
+        User Page 🔐
+      </NuxtLink>
+      <NuxtLink to="/protected">
+        Protected page 🔐
+      </NuxtLink>
+      <NuxtLink to="/about">
+        About page
+      </NuxtLink>
+    </template>
+
+    <template v-if="isTest || isGlobalMiddleware">
+      <NuxtLink
+        id="allow-all"
+        to="/global/allow/all"
+      >
+        Allow all
+      </NuxtLink>
+      <NuxtLink
+        id="allow-logged-in"
+        to="/global/allow/logged-in"
+      >
+        Allow logged-in
+      </NuxtLink>
+      <NuxtLink
+        id="allow-logged-out"
+        to="/global/allow/logged-out"
+      >
+        Allow logged-out
+      </NuxtLink>
+      <NuxtLink
+        id="deny-logged-in"
+        to="/global/deny/logged-in"
+      >
+        Deny logged-in
+      </NuxtLink>
+      <NuxtLink
+        id="deny-logged-out"
+        to="/global/deny/logged-out"
+      >
+        Deny logged-out
+      </NuxtLink>
+      <NuxtLink
+        id="incorrect-usage"
+        to="/global/incorrect-usage"
+      >
+        Incorrect usage
+      </NuxtLink>
+    </template>
   </nav>
   <NuxtPage />
 </template>
